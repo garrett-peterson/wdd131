a76217c7ca395faf9e122a4466d9c7865e2056ae
garrett-peterson <garrett.peterson726@gmail.com>
2026-02-03 13:56:19 -0600

    Did the BOM copy exercise

---
+++ week05/scripts/bom.js
@@ -0,0 +1,57 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+  displayList(chapter);
+});
+
+button.addEventListener('click', function() {
+  if (input.value != '') {
+    displayList(input.value);
+    chaptersArray.push(input.value);
+    setChapterList();
+    input.value = '';
+    input.focus();
+  }
+});
+
+function displayList(item) {
+    let li = document.createElement('li');
+    let deleteButton = document.createElement('button');
+
+    li.textContent = item;
+    deleteButton.textContent = '❌';
+    deleteButton.classList.add('delete');
+
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListener('click', function() {
+      list.removeChild(li);
+      deleteChapter(item);
+      input.focus();
+      input.value = '';
+
+  })
+};
+
+function setChapterList() {
+  localStorage.setItem("chaptersArray-ls", JSON.stringify(chaptersArray));
+};
+
+function getChapterList() {
+  return JSON.parse(localStorage.getItem('chaptersArray-ls'));
+}
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter((item) => item != chapter);
+  setChapterList();
+}
+
+
+input.focus();
+
